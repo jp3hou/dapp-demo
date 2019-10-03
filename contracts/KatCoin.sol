2b090bfb273291e3ab27e05742b7a2ad24e521f1
Julia Chou <julia.chou@coinbase.com>
2019-10-03 06:38:07 -0400

    Better naming

--- contracts/KatCoin.sol
+++ contracts/KatCoin.sol
@@ -11,8 +11,8 @@ contract KatCoin is Ownable {
   // @dev This event is emitted whenever a new KatCoin is minted
   event Minted(address owner, uint256 katId, uint256 genes);
 
-  // @dev This event is emitted when a token has been sent
-  event Sent(address from, address to, uint256 tokenId);
+  // @dev This event is emitted when a kat has been sent
+  event Sent(address from, address to, uint256 katId);
 
   struct Kat {
     uint32 color;
@@ -21,11 +21,11 @@ contract KatCoin is Ownable {
 
   Kat[] kats;
 
-  // Mapping from token ID to owner
-  mapping (uint256 => address) private _tokenOwner;
+  // Mapping from kat ID to owner
+  mapping (uint256 => address) private _katOwner;
 
   // Mapping from owner to number of owned KatCoins
-  mapping (address => uint256) private _ownedTokensCount;
+  mapping (address => uint256) private _ownedKatsCount;
 
   // Minting 30 KatCoins upon the initialization of this contract
   // (For the purposes of this demonstration only)
@@ -43,16 +43,16 @@ contract KatCoin is Ownable {
    */
   function balanceOf(address owner) public view returns (uint256) {
     require(owner != address(0));
-    return _ownedTokensCount[owner];
+    return _ownedKatsCount[owner];
   }
 
   /**
-   * @dev Gets the owner of the specified token ID
-   * @param tokenId uint256 ID of the token to query the owner of
-   * @return owner address currently marked as the owner of the given token ID
+   * @dev Gets the owner of the specified kat ID
+   * @param katId uint256 ID of the kat to query the owner of
+   * @return owner address currently marked as the owner of the given kat ID
    */
-  function ownerOf(uint256 tokenId) public view returns (address) {
-    address owner = _tokenOwner[tokenId];
+  function ownerOf(uint256 katId) public view returns (address) {
+    address owner = _katOwner[katId];
     require(owner != address(0));
     return owner;
   }
@@ -67,53 +67,53 @@ contract KatCoin is Ownable {
   function purchase(uint256 katId) public payable {
     require(katId >= 0);
     require(katId < 30);
-    require(address(this) == _tokenOwner[katId]);
+    require(address(this) == _katOwner[katId]);
     require(msg.value >= 200 finney);
     _transferFrom(address(this), msg.sender, katId);
     owner.transfer(msg.value);
   }
 
-  function _transferFrom(address from, address to, uint256 tokenId) internal {
-    require(from == _tokenOwner[tokenId]);
+  function _transferFrom(address from, address to, uint256 katId) internal {
+    require(from == _katOwner[katId]);
     require(to != address(0));
 
-    _removeTokenFrom(from, tokenId);
-    _addTokenTo(to, tokenId);
+    _removeKatFrom(from, katId);
+    _addKatTo(to, katId);
 
-    emit Sent(from, to, tokenId);
+    emit Sent(from, to, katId);
   }
 
   /**
-   * @dev Internal function to mint a new token
-   * Reverts if the given token ID already exists
-   * @param to The address that will own the minted token
-   * @param tokenId uint256 ID of the token to be minted by the msg.sender
+   * @dev Internal function to mint a new kat
+   * Reverts if the given kat ID already exists
+   * @param to The address that will own the minted kat
+   * @param katId uint256 ID of the kat to be minted by the msg.sender
    */
-  function _mint(address to, uint256 tokenId, uint256 genes) internal {
+  function _mint(address to, uint256 katId, uint256 genes) internal {
     require(to != address(0));
-    _addTokenTo(to, tokenId);
-    emit Minted(to, tokenId, genes);
+    _addKatTo(to, katId);
+    emit Minted(to, katId, genes);
   }
 
   /**
-   * @dev Internal function to add a token ID to the list of a given address
-   * @param to address representing the new owner of the given token ID
-   * @param tokenId uint256 ID of the token to be added to the tokens list of the given address
+   * @dev Internal function to add a kat ID to the list of a given address
+   * @param to address representing the new owner of the given kat ID
+   * @param katId uint256 ID of the kat to be added to the kats list of the given address
    */
-  function _addTokenTo(address to, uint256 tokenId) internal {
-    require(_tokenOwner[tokenId] == address(0));
-    _tokenOwner[tokenId] = to;
-    _ownedTokensCount[to]++;
+  function _addKatTo(address to, uint256 katId) internal {
+    require(_katOwner[katId] == address(0));
+    _katOwner[katId] = to;
+    _ownedKatsCount[to]++;
   }
 
   /**
-   * @dev Internal function to remove a token ID from the list of a given address
-   * @param from address representing the previous owner of the given token ID
-   * @param tokenId uint256 ID of the token to be removed from the tokens list of the given address
+   * @dev Internal function to remove a kat ID from the list of a given address
+   * @param from address representing the previous owner of the given kat ID
+   * @param katId uint256 ID of the kat to be removed from the kats list of the given address
    */
-  function _removeTokenFrom(address from, uint256 tokenId) internal {
-    require(ownerOf(tokenId) == from);
-    _ownedTokensCount[from]--;
-    _tokenOwner[tokenId] = address(0);
+  function _removeKatFrom(address from, uint256 katId) internal {
+    require(ownerOf(katId) == from);
+    _ownedKatsCount[from]--;
+    _katOwner[katId] = address(0);
   }
 }
